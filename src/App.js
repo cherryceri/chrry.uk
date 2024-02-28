f73b80edefa8947a6d795739214fccb0b92d7d7d
Cherry Walker <lukeww101@gmail.com>
2024-02-28 21:34:29 +0000

    Tweaked Icons & Colours, Fixed Meta Data

--- src/App.js
+++ src/App.js
@@ -3,15 +3,21 @@ import * as Components from './components/components.js';
 
 export default function App() {
     return (
-        <div className="App bg-gradient-to-tl from-dark to-darker bg-cover background-animate">
+        <div className="App bg-gradient-to-br from-neutral to-darker bg-cover background-animate">
             <div className="flex container flex-col h-screen relative bottom-[10vh] justify-center mx-auto items-center">
-                <img className="w-20 relative transition-all duration-750" src="/logo.png"></img>
+
+                <img className="w-20 relative brightness-75 animate-pulse" src="/logo.png"></img>
                 <Components.title text="cherry" />
+
                 <p className="tracking-widest text-secondary animate-pulse mt-[-2px]">Under Construction</p>
+
                 <div className="w-[100px] h-[2px] bg-tertiary mt-3"></div>
-                <div className="flex container flex-row items-center justify-center text-neutral text-3xl space-x-3 mt-1.5">
-                    <a href="https://www.youtube.com/channel/UCFdFRJM_tyKdw3TdBRqi9vw" className="hover:brightness-150 transition-all duration-25"><span className="mdi mdi-youtube"></span></a>
-                    <a href="https://twitter.com/cerise_cherry" className="hover:brightness-150 transition-all duration-25"><span className="mdi mdi-twitter"></span></a>
+
+                <div className="flex container flex-row items-center justify-center text-neutral text-3xl space-x-0.5 mt-1.5">
+                    <a title="YouTube" href="https://www.youtube.com/channel/UCFdFRJM_tyKdw3TdBRqi9vw" className="hover:brightness-200 transition-all duration-15"><span className="mdi mdi-play-box"></span></a>
+                    <a title="X" href="https://twitter.com/cerise_cherry" className="hover:brightness-200 transition-all duration-15"><span className="mdi mdi-file-excel-box"></span></a>
+                    <a title="GitHub" href="https://github.com/cherryceri" className="hover:brightness-200 transition-all duration-15"><span className="mdi mdi-code-braces-box"></span></a>
+                    <a title="AMP Server" href="https://amp.chrry.uk" className="hover:brightness-200 transition-all duration-15"><span className="mdi mdi-earth-box"></span></a>
                 </div>
             </div>
         </div >
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,11 +8,14 @@ module.exports = {
     extend: {
       colors: {
         primary: '#FEE2C9',
+        primary2: '#FFC285',
         secondary: '#EE8B88',
+        secondary2: '#FA387F',
         tertiary: '#553F4B',
+        tertiary2: '#612E53',
         neutral: '#2A2C3C',
         dark: '#111118',
-        darker: '#070f19',
+        darker: '#0A0411',
       },
     },
   },
