51c27ade177316a957ff0cc10676c81e07539e5a
Cherry Walker <lukeww101@gmail.com>
2024-06-10 18:12:14 +0100

    Added drip animation

--- app/socials/page.tsx
+++ app/socials/page.tsx
@@ -3,22 +3,26 @@ import Link from 'next/link'
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-12 lg:p-24">
-      <div className="mb-8">
-        <Link className="group absolute top-4 left-5 md:left-10 md:top-8 tracking-widest" href="/">
+      <div className="mb-8 w-[333px] md:w-[666px] relative">
+        <div className="text-6xl w-4 h-16 right-[2.75rem] md:right-[8.85rem] top-[6rem] md:top-[9.85rem] z-5 absolute mt-[-3rem] mb-[-1rem] animated-droplet cursor-default">.</div>
+
+        <Link className="group fixed top-4 left-5 md:left-10 md:top-8 tracking-widest" href="/">
           <h2 className="mb-3 text-4xl font-semibold transition-transform group-hover:translate-x-[-0.5rem]">
             <span className="inline-block transform scale-x-[-1] transition-transform">
               -&gt;
             </span>
           </h2>
         </Link>
-        <div className="z-10 w-auto text-center justify-between font-nunito text-[80px] md:text-[128px] font-bold tracking-widest">
-          <h1 className="top-0 animated-title">cherry</h1>
+        <div className="z-10 w-auto text-center justify-between font-nunito text-[80px] md:text-[128px] font-bold tracking-[0.15em]">
+          <h1 className="top-0 animated-title cursor-default z-10">cherry</h1>
         </div>
-        <p className="relative mb-[5vh] flex backdrop-blur-2xl w-fit m-auto rounded-xl border-solid border-2 bg-base3 border-base4 py-4 px-6">
+
+        <p className="relative mb-16 flex backdrop-blur-2xl w-fit m-auto rounded-xl border-solid border-2 bg-base3 border-base4 py-4 px-6">
           <code className="pl-1 font-mono font-bold typewriter tw20">app/chrry.uk/socials</code>
         </p>
 
-        <div className="relative mb-[5vh] flex backdrop-blur-2xl w-fill m-auto rounded-xl border-solid border-2 bg-base3 border-base4 pt-2 pb-4 px-6">
+
+        <div className="relative mb-16 flex backdrop-blur-2xl w-fill m-auto rounded-xl border-solid border-2 bg-base3 border-base4 pt-2 pb-4 px-6">
           <div className="bio pl-1 font-nunito text-lg tracking-wide">
             <span className="one">these are my personal & hobby links.</span>
             <span className="two">they do not at all represent me professionally.</span>
@@ -40,7 +44,6 @@ export default function Home() {
               <path d="M 44.898438 14.5 C 44.5 12.300781 42.601563 10.699219 40.398438 10.199219 C 37.101563 9.5 31 9 24.398438 9 C 17.800781 9 11.601563 9.5 8.300781 10.199219 C 6.101563 10.699219 4.199219 12.199219 3.800781 14.5 C 3.398438 17 3 20.5 3 25 C 3 29.5 3.398438 33 3.898438 35.5 C 4.300781 37.699219 6.199219 39.300781 8.398438 39.800781 C 11.898438 40.5 17.898438 41 24.5 41 C 31.101563 41 37.101563 40.5 40.601563 39.800781 C 42.800781 39.300781 44.699219 37.800781 45.101563 35.5 C 45.5 33 46 29.398438 46.101563 25 C 45.898438 20.5 45.398438 17 44.898438 14.5 Z M 19 32 L 19 18 L 31.199219 25 Z"></path>
             </svg>
           </a>
-
           <a
             href="https://www.twitch.tv/cherikaeru"
             className="group overflow-hidden relative border-2 border-base3 rounded-xl px-5 py-6 transition-colors bg-base hover:bg-base3"
@@ -56,7 +59,7 @@ export default function Home() {
           </a>
 
           <a
-            href="https://twitter.com/cerise_cherry"
+            href="https://www.allaboutbirds.org/news/"
             className="group overflow-hidden relative border-2 border-base3 rounded-xl px-5 py-6 transition-colors  bg-base hover:bg-base3"
             target="_blank"
             rel="noopener noreferrer"
